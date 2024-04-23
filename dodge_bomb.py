51027234556982efe9e89a0834a6b71a9df6a88a
shinnosuke <syunkun1973@gmail.com>
2024-04-23 15:27:22 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -7,6 +7,19 @@ import random
 WIDTH, HEIGHT = 1200, 600
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect，または，爆弾Rectの画面内外判定用の関数
+    引数：こうかとんRect,または，爆弾Rect
+    戻り値：横方向判定結果，縦方向判定結果（True:画面内/False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -15,7 +28,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bd_img = pg.Surface((20, 20))
+    bd_img = pg.Surface((20, 20)) #爆弾の設定
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
@@ -56,9 +69,16 @@ def main():
         #if key_lst[pg.K_RIGHT]:
         #    sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bd_rct.move_ip(vx, vy)
+        bd_rct.move_ip(vx, vy) #爆弾の表示
         screen.blit(bd_img, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         pg.display.update()
         pg.display.update()
         tmr += 1
